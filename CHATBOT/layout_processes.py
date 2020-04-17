77331837aa055814c315f3446efba0ee01af7405
smart-programmer <albrns1123488@outlook.com>
2020-04-17 08:44:01 +0300

    v7

--- CHATBOT/layout_processes.py
+++ CHATBOT/layout_processes.py
@@ -8,8 +8,8 @@ def show_text_process(client, text, conversation_session):
     conversationObj_id = conversation_session.conversationObj_id
     channelObj_id = conversation_session.contact.channel.channelObj_id
 
-    message = client.conversation_create_message(conversationObj_id, {
-  'channelId': channelObj_id,
+    message = client.conversation_create_message("8191d282593f49809be22df4394e4c0a", {
+  'channelId': '7e4da85010004d32a1427e4a2edcee33',
   'type': conversation_message.MESSAGE_TYPE_TEXT, 
   'content': {
     'text': text 
