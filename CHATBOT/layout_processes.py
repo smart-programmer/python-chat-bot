f4345936ac5e5ca477d028526deb8dc926d2b129
smart-programmer <albrns1123488@outlook.com>
2020-04-18 10:24:27 +0300

    f

--- CHATBOT/layout_processes.py
+++ CHATBOT/layout_processes.py
@@ -16,8 +16,8 @@ def show_text_process(client, text, conversation_session):
   #     'text': text 
   #   }
   # })# return a boolean if the mssage got sent or not and any time the message fails the layout should either redo the step that or delete everything
-  msg = client.conversation_create_message('8191d282593f49809be22df4394e4c0a', {
-        'channelId': '7e4da85010004d32a1427e4a2edcee33',
+  msg = client.conversation_create_message(conversationObj_id, {
+        'channelId': channelObj_id,
         'type': MESSAGE_TYPE_TEXT, 
         'content': {
             'text': str(text)
