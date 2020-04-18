26a16a5d51e6661adff6ea7b955d078da90479b4
smart-programmer <albrns1123488@outlook.com>
2020-04-18 09:28:39 +0300

    f

--- CHATBOT/layout_processes.py
+++ CHATBOT/layout_processes.py
@@ -1,5 +1,5 @@
 from CHATBOT import db
-from messagebird import conversation_message 
+
 from messagebird.conversation_message import MESSAGE_TYPE_HSM, MESSAGE_TYPE_TEXT
 
 # layout processes are reuseable functions and are the building blocks of layouts
@@ -9,13 +9,13 @@ def show_text_process(client, text, conversation_session):
   conversationObj_id = conversation_session.conversationObj_id
   channelObj_id = conversation_session.contact.channel.channelObj_id
 
-#     message = client.conversation_create_message("8191d282593f49809be22df4394e4c0a", {
-#   'channelId': '7e4da85010004d32a1427e4a2edcee33',
-#   'type': conversation_message.MESSAGE_TYPE_TEXT, 
-#   'content': {
-#     'text': text 
-#   }
-# })
+  #     message = client.conversation_create_message("8191d282593f49809be22df4394e4c0a", {
+  #   'channelId': '7e4da85010004d32a1427e4a2edcee33',
+  #   'type': conversation_message.MESSAGE_TYPE_TEXT, 
+  #   'content': {
+  #     'text': text 
+  #   }
+  # })# return a boolean if the mssage got sent or not and any time the message fails the layout should either redo the step that or delete everything
   msg = client.conversation_create_message('8191d282593f49809be22df4394e4c0a', {
           'channelId': '7e4da85010004d32a1427e4a2edcee33',
           'type': MESSAGE_TYPE_TEXT, 
@@ -23,4 +23,4 @@ def show_text_process(client, text, conversation_session):
               'text': str(text)
           }
 
-    # return a boolean if the mssage got sent or not and any time the message fails the layout should either redo the step that or delete everything
+  
