c476044bbddcc4ce061368d03f61a8b0223b3b7f
smart-programmer <albrns1123488@outlook.com>
2020-04-18 07:46:50 +0300

    f

--- CHATBOT/routes.py
+++ CHATBOT/routes.py
@@ -26,13 +26,14 @@ def webhook():
         webhookOBJ.messagebird_request_string = webhook_json_string
         db.session.add(webhookOBJ)
         db.session.commit()
-        client = Client(message_bird_api_access_key, features=[messagebird.Feature.ENABLE_CONVERSATIONS_API_WHATSAPP_SANDBOX])
-        msg = client.conversation_create_message('8191d282593f49809be22df4394e4c0a', {
-  'channelId': '7e4da85010004d32a1427e4a2edcee33',
-  'type': MESSAGE_TYPE_TEXT, 
-  'content': {
-    'text': "هلاااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااا"
-  }
+        client = messagebird.Client('4LyuUQ5rrh2CT1Zwrql1hYuBW', features=[messagebird.Feature.ENABLE_CONVERSATIONS_API_WHATSAPP_SANDBOX])
+        if not request.json["type"] == "sent":
+            msg = client.conversation_create_message('8191d282593f49809be22df4394e4c0a', {
+    'channelId': '7e4da85010004d32a1427e4a2edcee33',
+    'type': MESSAGE_TYPE_TEXT, 
+    'content': {
+        'text': "هلاااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااا"
+    }
 })
 
     return "<h1>{}</h1>".format(str([i.messagebird_request_string for i in WebhookMessage.query.all()]))
