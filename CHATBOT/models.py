a6d0070d1f8f50903489270682119b42ad2f5a25
smart-programmer <albrns1123488@outlook.com>
2020-04-24 08:54:27 +0300

    f

--- CHATBOT/models.py
+++ CHATBOT/models.py
@@ -21,7 +21,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(60), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     number = db.Column(db.String(20), nullable=False, unique=True)
     full_name = db.Column(db.String(255), nullable=False, unique=True)
